561e25d5cc8af514c90232ea6a6ad813a8adb40e
rajkdh <112664790+rajkdh@users.noreply.github.com>
2023-05-20 22:33:39 -0400

    added code to render license link based on the license selected

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,7 +2,7 @@
 // If there is no license, return an empty string 
 //.each for each license selected?
 function renderLicenseBadge(license) {
-if (license !== 'none') {
+if (license !== 'None') {
     return `[![License: ${license}](https://img.shields.io/badge/License-${license}-yellow.svg)](https://opensource.org/licenses/${license})`;
   } else {
    return '';
@@ -11,7 +11,13 @@ if (license !== 'none') {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== 'None') {
+    return `https://opensource.org/licenses/${license}`;
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
