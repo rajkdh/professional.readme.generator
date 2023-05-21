feb30ef5f7c79ef8f1cd5969bc176198483bfe45
rajkdh <112664790+rajkdh@users.noreply.github.com>
2023-05-21 00:33:25 -0400

    fixed bug

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -22,7 +22,7 @@ function renderLicenseLink(license) {
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  if (licence !== 'None') {
+  if (license !== 'None') {
     return `## License
     
     This project is licensed under the ${license}license.`
