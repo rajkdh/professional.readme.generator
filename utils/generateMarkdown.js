303fceb189c5e6dde789191577421e8dcf96aaf6
rajkdh <112664790+rajkdh@users.noreply.github.com>
2023-05-20 23:30:18 -0400

    added code to structure the generate readme file with appropriate layout:

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -15,7 +15,7 @@ function renderLicenseLink(license) {
   if (license !== 'None') {
     return `https://opensource.org/licenses/${license}`;
   } else {
-    return '';
+    return ''
   }
 }
 
@@ -25,7 +25,43 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return ` ${renderLicenseBadge(data.license)}
+
+<h3 align="center">${data.title}</h3>
+
+<details>
+  <summary>Table of Contents</summary>
+  <ol>
+    <li>
+        <a href="#about-the-project">About The Project</a>
+      <ul>
+        <li><a href="#installation">Installation</a></li>
+      </ul>
+    </li>
+    <li><a href="#usage">Usage</a></li>
+    <li><a href="#credit">Credit</a></li>
+    <li><a href="#license">License</a></li>
+  </ol>
+</details>
+
+## About The Project
+${data.description}
+
+## Installation
+Following dependecies are required:
+\`\`\`
+${data.installation}
+\`\`\`
+
+## Usage
+${data.usage}
+
+## Credit
+${data.credit}
+
+## License
+Distributed under the ${data.license} License. </br>
+To see the license permissions for commercial and non-commercial use, modification, and distribution of the software, please see the full text of the license, available at https://opensource.org/licenses/${data.license}.
 
 `;
 }
